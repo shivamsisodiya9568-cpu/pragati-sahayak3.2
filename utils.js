c12ebcf5b73d95d72f768756da4844b2d796a164
shivamsisodiya9568-cpu <shivamsisodiya9568@gmail.com>
2026-08-05 21:10:31 +0530

    Delete utils.js

--- utils.js
+++
@@ -1,6 +0,0 @@
-/* ==========================================================================
-   Pragati Sahayak - Utility Functions
-   ========================================================================== */
-
-// Placeholder for future utility functions like formatting, validations, etc.
-console.log("Utils loaded.");
